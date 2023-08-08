4c8519d63a3ad936146192fc267ceeb9928a8ff6
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2023-08-08 14:15:55 +0530

    Update AMI-backup-script.py

--- AMI-backup-script.py
+++ AMI-backup-script.py
@@ -2,14 +2,13 @@ import boto3
 import datetime
 import os
 
-def delete_ami(ec2_client, name, date):
+def delete_ami(ec2_client, name, date, delToday):
     try:
-        describeImages = ec2_client.describe_images(Filters=[{'Name': 'tag:Name', 'Values': ['AMIBackup-' + name]}])['Images']
-        print(describeImages)
+        describeImages = ec2_client.describe_images(Filters=[{'Name': 'tag:Name', 'Values': ['AMIBackup-' + name + "-" + delToday]}])['Images']
 
+        
         for image in describeImages:
             imageId = image['ImageId']
-            print("imageId --->" + imageId)
 
             if 'BlockDeviceMappings' not in image:
                 print(f"BlockDeviceMappings not found for AMI {imageId}")
@@ -20,23 +19,20 @@ def delete_ami(ec2_client, name, date):
                 if 'Ebs' in mapping and 'SnapshotId' in mapping['Ebs']:
                     snapList.append(mapping['Ebs']['SnapshotId'])
 
-            print(snapList)
 
             tagDelDate = None  # Default value for tagDelDate
             for tag in image['Tags']:
                 try:
                     if tag['Key'] == 'DeleteOn':
                         tagDelDate = tag['Value']
-                        print("tagDelDate --->" + tagDelDate)
+                        
                 except Exception as e:
                     print('Tag Errors: ', e)
 
-            if tagDelDate == date:
-                print('Deregistering image ---> ', imageId)
+            if tagDelDate == date :
                 dereg_img = ec2_client.deregister_image(ImageId=imageId)
 
                 for snapId in snapList:
-                    print('Deleting Snapshot: ', snapId)
                     del_snap = ec2_client.delete_snapshot(SnapshotId=snapId)
             else:
                 print('Image cannot be deleted: ', imageId)
@@ -47,35 +43,32 @@ def delete_ami(ec2_client, name, date):
 
 def lambda_handler(event, context):
     try:
-        # instanceList = [os.environ['INSTANCE1'], os.environ['INSTANCE2'], os.environ['INSTANCE3'], os.environ['INSTANCE4']] 
-        # regions = [os.environ['REGION1'], os.environ['REGION2']]
-
         server_dictionary = {
-                                os.environ['REGION1'] : [os.environ['INSTANCE1'], os.environ['INSTANCE2']], 
+                                os.environ['REGION1'] : [os.environ['test1'], os.environ['test2'], os.environ['test3'], os.environ['test4'], os.environ['test5'], os.environ['test6'], os.environ['test7'], os.environ['test8'], os.environ['test9'], os.environ['test10']], 
                                 os.environ['REGION2'] : [os.environ['INSTANCE3'], os.environ['INSTANCE4']]
                             }
         
-        
         currentDT = datetime.datetime.now()
         date = currentDT.strftime("%d-%m-%Y")
-        delDate = currentDT + datetime.timedelta(days=2)
+        delDate = currentDT + datetime.timedelta(days=1)
         delDate = delDate.strftime("%d-%m-%Y")
+        delToday = (currentDT - datetime.timedelta(days=1)).strftime("%d-%m-%Y")
+        
+        
 
         for region in list(server_dictionary.keys()):
-            print(f"Processing region: {region}")
             ec2_client = boto3.client('ec2', region_name=region)
 
             response = ec2_client.describe_instances(InstanceIds= server_dictionary[region])  
-            print(response)
             for instance in response['Reservations']:
                 for i in instance['Instances']:
                     try:
                         tagList = i.get('Tags', '')
-
+                        
                         for tag in tagList:
                             if tag.get('Key', '') == 'Name':
                                 name = tag.get('Value', '')
-                                delete_ami(ec2_client, name, date)
+                                delete_ami(ec2_client, name, date, delToday)
                                 break
 
                         createImage = ec2_client.create_image(InstanceId=i.get('InstanceId', ''),
@@ -84,10 +77,9 @@ def lambda_handler(event, context):
                                                               NoReboot=True, DryRun=False)
 
                         createTags = ec2_client.create_tags(Resources=[createImage.get('ImageId', '')],
-                                                            Tags=[{'Key': 'Name', 'Value': 'AMIBackup-' + name},
+                                                            Tags=[{'Key': 'Name', 'Value': 'AMIBackup-' + name +"-"+date},
                                                                   {'Key': 'DeleteOn', 'Value': delDate}])
 
-                        print('Image Created: ', createImage)
 
                     except Exception as e:
                         print('Image Creation Errors: ', e)
@@ -97,4 +89,3 @@ def lambda_handler(event, context):
     except Exception as e:
         print('Errors: ', e)
 
-lambda_handler(None, None)  # Invoke the lambda function (This is only for local testing)
